f216414c447fb4311989b3fcfca3804d46af71f3
Carl Hester <crucialcarl@gmail.com>
2019-10-25 00:59:22 +0000

    growing

--- jsontest.go
+++ jsontest.go
@@ -4,24 +4,45 @@ import "fmt"
 import "encoding/json"
 
 var DATA string = ` 
-              { "estimate":{
-	                "minutes": "2222",
-	                "direction": "North"
-	              }
-              }`
+              {
+				"name": "Montgomery St.",
+				"abbr": "MONT",
+				"etd": [{
+					"destination": "Antioch",
+					"abbreviation": "ANTC",
+					"limited": "0",
+					"estimate":[ {
+	            	    "minutes": "2222",
+	            	    "direction": "North"
+	            	  },{
+	            	    "minutes": "3333",
+	            	    "direction": "South"
+						}]
+				}]}`
 
-type Estimates struct {
+type Estimates []struct {
 	Minutes   string `json:"minutes"`
 	Direction string `json:"direction"`
 }
 
-type Etd struct {
-	Est Estimates `json:"estimate"`
+type Etd []struct {
+	Destination  string    `json:"destination"`
+	Abbreviation string    `json:"abbreviation"`
+	Limited      int       `json:"limited"`
+	Est          Estimates `json:"estimate"`
+}
+
+type Station struct {
+	Name string `json:"name"`
+	Abbr string `json:"abbr"`
+	Etd  Etd    `json:"etd"`
 }
 
 func main() {
 	//fmt.Println(DATA)
-	var etd Etd
-	json.Unmarshal([]byte(DATA), &etd)
-	fmt.Printf("%+v\n", etd)
+	//var etd Etd
+	var station Station
+	json.Unmarshal([]byte(DATA), &station)
+	fmt.Printf("%+v\n", station)
+	fmt.Println(station)
 }
